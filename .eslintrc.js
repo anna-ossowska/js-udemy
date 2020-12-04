c427afd2b231f1bb27df87dd0ba477e907a0d89b
Anna Ossowska <ossowska.anna1@gmail.com>
2020-12-04 15:34:25 +0100

    Configuring ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: ['error', 2],
+    semi: [2, 'always'],
+    allowTemplateLiterals: true
+  }
+};
